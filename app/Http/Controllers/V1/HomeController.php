c2526aa599180982452ad2fbdc6be0e325a02f69
Fahmi <fahmi@appwapp.com>
2022-01-09 18:10:55 -0500

    Ebauche pour l'envoie des mail plus tard

--- app/Http/Controllers/V1/HomeController.php
+++ app/Http/Controllers/V1/HomeController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers\V1;
 
+use App\Mail\OrderShipped;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
-    public function __invoke(): array{
+    public function __invoke() {
+        /*Mail::to("fahmiderbali@gmail.com")
+            ->from("noreply@funnymemes.com")
+            ->send(new OrderShipped());
+        return new OrderShipped();*/
         return [
             'success' => true,
             'message' => __('messages.welcome'),

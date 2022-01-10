9dcdbf30b0eb45e5e113a12065eb709134ee0b37
Fahmi <fahmi@appwapp.com>
2022-01-09 21:34:20 -0500

    Authentification via JWT

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\V1\HomeController;
+use App\Http\Controllers\V1\AuthController;
 
 Route::get('/', HomeController::class);
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
